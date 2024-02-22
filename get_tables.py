5d8be508b0aae4f19b411c7405edc71edb27a572
Jason Zhang <jasonyzhang@cmu.edu>
2024-02-21 23:32:01 -0500

    first commit

---
+++ get_tables.py
@@ -0,0 +1,32 @@
+"""
+python resources/get_tables.py --directory resources/co3d_result
+"""
+
+import argparse
+import os.path as osp
+import random
+from glob import glob
+
+
+TABLE_HTML = """<div class="item-{ind}"><table width="400px" class="border"><tr><td><video autoplay loop muted playsinline controls width="100%"><source src="{video_path}" type="video/mp4"> </video> </td> </tr> <tr> <td> <img src="{image_path}" width="100%"></img> </td></tr></table></div>"""
+
+
+def get_tables(directory, random_order=False):
+    images = sorted(glob(osp.join(directory, "*.png")))
+    videos = sorted(glob(osp.join(directory, "*.mp4")))
+    assert len(images) == len(videos)
+    order = list(range(len(images)))
+    if random_order:
+        random.shuffle(order)
+
+    for i in range(len(images)):
+        image = images[order[i]]
+        video = videos[order[i]]
+        print(TABLE_HTML.format(ind=i + 1, image_path=image, video_path=video))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", type=str, required=True)
+    args = parser.parse_args()
+    get_tables(args.directory, True)
---
+++ process_videos.py
@@ -0,0 +1,39 @@
+import os.path as osp
+import subprocess
+from glob import glob
+
+
+INPUT_DIR = "resources/co3d_results"
+OUTPUT_DIR = "resources/co3d_results_processed"
+
+
+def process_images(input_dir, output_dir):
+    for image in glob(osp.join(input_dir, "*.png")):
+        output_image = osp.join(output_dir, osp.basename(image).replace(".png", ".jpg"))
+        cmd = ["convert", image, "-resize", "400x400", output_image]
+        print(" ".join(cmd))
+        subprocess.call(cmd)
+
+
+def process_videos(input_dir, output_dir):
+    for video in glob(osp.join(input_dir, "*.mp4")):
+        output_video = osp.join(output_dir, osp.basename(video))
+        cmd = [
+            "ffmpeg",
+            "-i",
+            video,
+            "-c:v",
+            "h264",
+            "-an",
+            "-vf",
+            "setpts=(2/3)*PTS,scale=400:400",
+            output_video,
+            "-y",
+        ]
+        print(" ".join(cmd))
+        subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    # process_images(INPUT_DIR, OUTPUT_DIR)
+    process_videos(INPUT_DIR, OUTPUT_DIR)
